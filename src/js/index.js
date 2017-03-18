0b60fdda95315c3211a6dd282a665f62ea046591
Alex Milanov <alex@i4web.biz>
2017-03-18 17:39:12 +0200

    initial protorype

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@
 const Rx = require('rx');
 const $ = Rx.Observable;
 
-const d3 = require('d3');
+let d3 = window.d3;
 
 // iblokz
 const vdom = require('iblokz/adapters/vdom');
@@ -42,11 +42,49 @@ const state$ = actions$
 	.map(state => (console.log(state), state))
 	.share();
 
+const renderChart = (el, analyser) => {
+	requestAnimationFrame(() => renderChart(el, analyser));
+	let frequencyData = new Uint8Array(200);
+	// copy frequency data to frequencyData array.
+	analyser.getByteFrequencyData(frequencyData);
+	// console.log(frequencyData);
+
+	// scale things to fit
+	let radiusScale = d3.scale.linear()
+			.domain([0, d3.max(frequencyData)])
+			.range([0, el.attr('height') / 2 - 10]);
+
+	let hueScale = d3.scale.linear()
+			.domain([0, d3.max(frequencyData)])
+			.range([0, 360]);
+
+	console.log(frequencyData);
+
+	// update d3 chart with new data
+	let circles = el.selectAll('circle')
+		.data(frequencyData);
+
+	circles.enter()
+		.append('circle');
+
+	circles
+		.attr({
+			'r': d => radiusScale(d),
+			'cx': el.attr('width') / 2,
+			'cy': el.attr('height') / 2,
+			'fill': 'none',
+			'stroke-width': 4,
+			'stroke-opacity': 0.4,
+			'stroke': d => d3.hsl(hueScale(d), 1, 0.5)
+		});
+
+	circles.exit().remove();
+};
+
 // d3 code here
 const initd3 = () => {
-	const dataTsv = "https://gist.githubusercontent.com/alex-milanov/eabaa766034d1094309f41994164f515/raw/26538cbd3afe46554d8f0fff24d2bd782e9bd017/data.tsv";
-	console.log(123);
 	const svg = d3.select("svg");
+	/*
 	const margin = {top: 20, right: 20, bottom: 30, left: 40};
 	const width = Number(svg.attr("width")) - margin.left - margin.right;
 	const height = Number(svg.attr("height")) - margin.top - margin.bottom;
@@ -55,42 +93,20 @@ const initd3 = () => {
 	const y = d3.scaleLinear().rangeRound([height, 0]);
 
 	const g = svg.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	*/
+	var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+	var audioElement = document.getElementById('song');
+	var audioSrc = audioCtx.createMediaElementSource(audioElement);
+	var analyser = audioCtx.createAnalyser();
 
-	d3.tsv(dataTsv, d => Object.assign({}, d, {frequency: Number(d.frequency)}),
-		(error, data) => {
-			if (error) throw error;
+	// bind our analyser to the media element source.
+	audioSrc.connect(analyser);
+	audioSrc.connect(audioCtx.destination);
 
-			x.domain(data.map(d => d.letter));
-			y.domain([0, d3.max(data, d => d.frequency)]);
+	// var frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-			// x axis
-			g.append("g")
-					.attr("class", "axis axis--x")
-					.attr("transform", "translate(0," + height + ")")
-					.call(d3.axisBottom(x));
-
-			// y axis
-			g.append("g")
-					.attr("class", "axis axis--y")
-					.call(d3.axisLeft(y).ticks(10, "%"))
-				.append("text")
-					.attr("transform", "rotate(-90)")
-					.attr("y", 6)
-					.attr("dy", "0.71em")
-					.attr("text-anchor", "end")
-					.text("Frequency");
-
-			// bars
-			g.selectAll(".bar")
-				.data(data)
-				.enter().append("rect")
-					.attr("class", "bar")
-					.attr("x", d => x(d.letter))
-					.attr("y", d => y(d.frequency))
-					.attr("width", x.bandwidth())
-					.attr("height", d => height - y(d.frequency));
-		});
+	renderChart(svg, analyser);
 };
 
 state$
